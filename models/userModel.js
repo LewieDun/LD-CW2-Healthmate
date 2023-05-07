8b38d759a07305b8f7fc705bd41c7d2c53adc3b4
Lewie Duncan <duncanlewie@intric-solutions.com>
2023-05-07 18:32:42 +0100

    Code Tidy Up

--- models/userModel.js
+++ models/userModel.js
@@ -15,31 +15,43 @@ class UserDAO {
     }
   }
 
-  // for the demo the password is the bcrypt of the user name
   init() {
+    // Set the username for the user we're looking for
     let myUser = "Lewie";
+
+    // Return a new Promise to handle the async DB call
     return new Promise((resolve, reject) => {
+      // Query the DB for entries with user = myUser
       this.db.find({ user: myUser }, (err, entries) => {
         if (err) {
+          // Reject the Promise if an error occurs
           reject(err);
-        } else if (entries != null && entries.some(e => e.user === myUser)) {
+        } else if (entries != null && entries.some((e) => e.user === myUser)) {
+          // If the user already exists, log a message and resolve the Promise
           console.log("User Already Inserted");
-          console.log(entries)
+          console.log(entries);
           resolve(this);
         } else {
-          this.db.insert({
-            firstname: "Lewie",
-            lastname: "Duncan",
-            user: "Lewie",
-            password:"$2a$12$b6Uu6KWKxSHm95lxsLmy7uxMI.BwI0te2tLye7NQRNQMpctTH5fnG",
-          }, (err) => {
-            if (err) {
-              reject(err);
-            } else {
-              console.log("User insert Done");
-              resolve(this);
+          // If the user doesn't exist, insert a new entry into the DB
+          this.db.insert(
+            {
+              firstname: "Lewie",
+              lastname: "Duncan",
+              user: "Lewie",
+              password:
+                "$2a$12$b6Uu6KWKxSHm95lxsLmy7uxMI.BwI0te2tLye7NQRNQMpctTH5fnG",
+            },
+            (err) => {
+              if (err) {
+                // Reject the Promise if an error occurs
+                reject(err);
+              } else {
+                // Log a message and resolve the Promise if the insert was successful
+                console.log("User insert Done");
+                resolve(this);
+              }
             }
-          });
+          );
         }
       });
     });
@@ -47,6 +59,7 @@ class UserDAO {
 
   create(firstname, lastname, username, password) {
     const that = this;
+    // Hash the password before inserting into the database
     bcrypt.hash(password, saltRounds).then(function (hash) {
       var entry = {
         firstname: firstname,
@@ -54,6 +67,7 @@ class UserDAO {
         user: username,
         password: hash,
       };
+      // Insert the user record into the database
       that.db.insert(entry, function (err) {
         if (err) {
           console.log("Can't insert user: ", username);
@@ -75,11 +89,15 @@ class UserDAO {
     });
   }
 }
-const dao = new UserDAO("user-database.db");
-dao.init().then((result) => {
-    console.log("Init completed:", result);
-  }).catch((err) => {
-    console.error("Init failed:", err);
+
+const dao = new UserDAO("user-database.db"); // Path for database
+dao
+  .init()
+  .then((result) => {
+    console.log("Init completed:", result); // print the result of init
+  })
+  .catch((err) => {
+    console.error("Init failed:", err); // otherwise print the error
   });
 
 module.exports = dao;
